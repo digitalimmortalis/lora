f9ef32c374c5695bd5d0572f741ec5e62f17c878
digitalimmortalis <104713936+digitalimmortalis@users.noreply.github.com>
2022-12-08 15:58:15 +0100

    Update run_lora_db.sh

--- run_lora_db.sh
+++ run_lora_db.sh
@@ -1,13 +1,13 @@
 #https://github.com/huggingface/diffusers/tree/main/examples/dreambooth
-export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
-export INSTANCE_DIR="./data_example"
-export OUTPUT_DIR="./output_example"
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export INSTANCE_DIR="/kaggle/input/valentinapiner48"
+export OUTPUT_DIR="/kaggle/working/checkpoints"
 
 accelerate launch train_lora_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="style of sks" \
+  --instance_prompt="a photo of valentinapiner" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
